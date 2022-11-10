5f08b4083c1e1e2db06e51f1937faedeca8c18b8
mateus-69 <mateusjbarbosa00@gmail.com>
2022-11-10 17:02:55 -0300

    asdsa

--- index.js
+++ index.js
@@ -27,3 +27,15 @@ submit.addEventListener("click", () => {
     
     var object = {"nome":nome, "sobrenome":sobrenome, "CPF":CPF, "email":email, "telefone":telefone};
 })
+
+//localStorage?
+var testObject = { 'one': 1, 'two': 2, 'three': 3 };
+
+// Put the object into storage
+localStorage.setItem('testObject', JSON.stringify(testObject));
+
+// Retrieve the object from storage
+var retrievedObject = localStorage.getItem('testObject');
+
+console.log('retrievedObject: ', JSON.parse(retrievedObject));
+
