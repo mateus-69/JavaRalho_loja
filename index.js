c30084228faa78ee12d2dc63526f1900b2bb54ed
DevBarbosa <104536028+mateus-69@users.noreply.github.com>
2022-11-10 12:26:41 -0300

    Add_img

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+<<<<<<< HEAD
 let submit = document.getElementById("submit");
+
+submit.addEventListener("click", () => {
+    alert(document.getElementById("nome").value);
+})
+
+//localStorage?
+var testObject = { 'one': 1, 'two': 2, 'three': 3 };
+=======
+let submit = document.getElementById("submit");
+>>>>>>> f24e04133c2c74450c606ae1e7685e9a20412139
 
 if (localStorage.getItem("Cadastros") == null){
     localStorage.setItem("Cadastros", "[]");
